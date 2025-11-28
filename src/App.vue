627259faef494aa41d648d0d869e4fcc2aae6291
Toki <ineshamid@gmail.com>
2025-11-28 22:05:43 +0100

    :tada: Created base for vue project

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import MainPageComponent from "@/components/main-page/MainPageComponent.vue";
+</script>
+
+<template>
+  <header>
+  </header>
+
+  <main>
+    <main-page-component></main-page-component>
+  </main>
+</template>
